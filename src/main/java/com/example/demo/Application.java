8cb2e0ef9c56f083f29018d28b83f8d9e2dc6f4d
saretoduncan <saretoduncan@gmail.com>
2021-09-12 10:05:36 +0300

    learn basics of spring jpa

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +11,11 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return  args -> {
+             Student maria = new Student( "Maria","Jones","maria.jones@gmail.com", 21);
+            studentRepository.save(maria);
+        };
+    }
 }
